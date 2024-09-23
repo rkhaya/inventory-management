2bd13589a421ced1014c662049c3ade3046504c7
rkhaya <61182873+rkhaya@users.noreply.github.com>
2024-09-23 16:05:08 -0500

    unused fix

--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -27,12 +27,15 @@ import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 /* REDUX PERSISTENCE */
 const createNoopStorage = () => {
   return {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     getItem(_key: any) {
       return Promise.resolve(null);
     },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     setItem(_key: any, value: any) {
       return Promise.resolve(value);
     },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     removeItem(_key: any) {
       return Promise.resolve();
     },
